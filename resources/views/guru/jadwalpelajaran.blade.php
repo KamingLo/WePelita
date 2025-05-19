2e6f368a79c71a4e2e4e664f9c8f7e08a841ad1d
Kaming Lo <lokaming86@gmail.com>
2025-05-19 13:10:49 +0700

    trash

---
+++ resources/views/guru/jadwalpelajaran.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jadwal Pelajaran</title>
+    @livewireStyles
+</head>
+<body>
+
+    <h1>Jadwal Pelajaran</h1>
+
+    @livewire('jadwal-pelajaran')
+
+    @livewireScripts
+</body>
+</html>
