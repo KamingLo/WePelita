0605f4a5da70927116b336fc42787f64f2ee1006
Kaming Lo <lokaming86@gmail.com>
2025-05-21 12:53:28 +0700

    jawa

--- database/factories/TahunAjarFactory.php
+++ database/factories/TahunAjarFactory.php
@@ -8,7 +8,7 @@ class TahunAjarFactory extends Factory
     public function definition(): array
     {
         // Contoh tahun ajaran format: "2023/2024"
-        $startYear = $this->faker->numberBetween(2018, 2024);
+        $startYear = $this->faker->numberBetween(2022, 2024);
         $endYear = $startYear + 1;
         return [
             'tahun_ajaran' => $startYear . '/' . $endYear,
