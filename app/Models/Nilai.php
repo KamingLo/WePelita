088fcac09b690a5a2e99d7a9b8e5852195b29223
Kaming Lo <lokaming86@gmail.com>
2025-05-29 15:26:56 +0700

    udah bisa up nilai

--- app/Models/Nilai.php
+++ app/Models/Nilai.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Nilai extends Model
 {
     use HasFactory;
-
+    protected $table = 'nilai';  // ini penting, supaya pakai tabel 'nilai' yang sudah ada
+    protected $primaryKey = 'nilai_id'; // kalau primary key bukan 'id', set juga ini
+    public $timestamps = false;  // kalau kamu tidak memakai timestamps
     protected $fillable = ['murid_kelas_id', 'pelajaran_id','nilai_tugas', 'nilai_uts', 'nilai_uas'];
 
     public function muridkelas()
--- routes/web.php
+++ routes/web.php
@@ -107,7 +107,9 @@ Route::middleware([RoleMiddleware::class.':guru'])->group(function() {
     Route::get('guru/jadwal', [GuruController::class, 'tampilkanJadwalPelajaran'])->name('guru.jadwal');
     Route::get('guru/jadwalanda', [GuruController::class, 'tampilkanJadwalAnda'])->name('guru.jadwalanda');
     Route::get('guru/buatpengumuman', [GuruController::class, 'tampilkanPengumuman'])->name('guru.pengumuman');
+
     Route::get('guru/menu-nilai', [GuruController::class, 'tampilkanMenuNilai'])->name('guru.isinilai');
+    Route::post('guru/menu-nilai', [GuruController::class, 'simpanNilai'])->name('guru.isinilai');
 
     Route::get('guru/ManajemenPostGuru', [GuruController::class, 'tampilkanManajemenPost'])->name('guru.manajemenPost');    
 
